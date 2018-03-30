e5bdab9de450d1374bdae39d5501780d7572eeb8
ortizk <ortizk@live.com>
2018-03-30 14:07:12 -0700

    button pushed, appends to the current li not making new one

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,1 +1,17 @@
-// Your jQuery Logic will go here
+console.log("js is working!");
+
+
+$('#postInputForm').on('submit', (e) => {
+	e.preventDefault();
+	console.log("button pushed");
+	console.log($('#postInput').val());
+	$('li').append($('#postInput').val());
+	
+	//.append($('<li></li>'));
+	// $('li').addTo('ul')
+
+
+	// $('li')append($("#postInput").val());
+});
+	// create new li
+	// append to ul
