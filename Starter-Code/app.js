33fc6abefbab02b4d763b87b8f5aff6d3a16b180
smithalex24 <smith25243@gmail.com>
2018-03-30 15:17:45 -0700

    Second commit

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,8 +1,45 @@
+let choreList = [];
+
 $(document).ready(function(){
   $("#cat").fadeIn(4000);
-
-  $('#myForm').submit(function(e){
-    e.preventDefault();
-    console.log('form was submitted with', $('#form1').val());
-  });
+ 
+	// const chores = function (event) {
+	$("#submitButton").click((event) => {
+	event.preventDefault();
+	let getChore = $('#form1').val();
+	let newChore = $('<li>');
+	// newChore.innerHTML = getChore;
+	$("#list").append(newChore);
+	$(newChore).html("Cheshire's chore is" + getChore);
+	$('#form1').val('');
+	
+})
 });
+
+
+const addToLocalStorage = (item) => {
+	choreList.push(item)
+	localStorage.setItem("LSChores", JSON.stringify(choreList));
+	console.log("adding to storage");
+};
+
+const getFromLocalStorage = () => {
+	console.log("getting from local storage");
+	choreList = JSON.parse(localStorage.getItem("LSChores")) || [];
+
+	console.log(choreList, typeof choreList);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
