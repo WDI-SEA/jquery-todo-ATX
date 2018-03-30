4eb0d77bf1016f7baa780eb4579eecba026a11bf
jessiheger <jhmheger@gmail.com>
2018-03-30 15:56:23 -0700

    first commit (super late, I know...)

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,1 +1,38 @@
 // Your jQuery Logic will go here
+
+// $('#addForm').submit(function (e) {
+//  e.preventDefault();
+//  console.log('submitted!');
+// });
+// const inputBox = $('addForm');
+
+$(document).ready(function(){
+$('button').click( (e) => {
+  e.preventDefault();
+  var taskText = $("#addTask").val();
+  var newLI = $('<li>');
+  newLI.on('click', liMove);
+  $('#outstanding').append(newLI);
+  $(newLI).html(taskText);
+  $('#addTask').val('');
+})
+});
+
+
+$(document).ready(function(){
+// $('li').click( (e) => {
+//  // e.preventDefault();
+//  var taskDone = e.target;
+//  var lIDone = document.createElement('li');
+//  $('#done').append(lIDone);
+//  $(lIDone).html(taskDone);
+// })
+})
+
+function liMove(e){
+  $(this).appendTo($('#done'));
+}
+
+// add click event to item in first column
+
+// once firstArray item is clicked, splice out of firstArray and add to secondArray.
