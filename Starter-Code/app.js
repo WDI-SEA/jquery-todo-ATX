3133c044cb5ed430e580bbfa7b7b07053dc8da7e
JackPound <jackpound@live.com>
2018-03-30 14:02:12 -0700

    form input works to create todo list

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,1 +1,31 @@
 // Your jQuery Logic will go here
+console.log("js linked");
+
+let submitForm = $("#submit-form");
+
+
+submitForm.submit(function(e) {
+	e.preventDefault();
+	let inputText = $("#input-box").val();
+	
+	$('#toDoList').append(
+    	$('<li>').append(inputText)      
+	);
+	document.forms['submit-form'].reset()
+});
+
+var element = document.getElementById('toDoList');
+
+$("<li>").click(function(element) {
+	// element.removeChild(element);
+	console.log("clicked list")
+})
+
+
+// submitForm.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     let blogPost = $('#input-box').value;
+//     posts.unshift(blogPost)
+//  	console.log(blogPost);
+//     document.forms['submit-form'].reset()
+//   });
