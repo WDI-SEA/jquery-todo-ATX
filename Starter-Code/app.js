1b7455c8f8f48dac4cec2f8bff206d397daee10f
ortizk <ortizk@live.com>
2018-03-30 14:39:38 -0700

    SHIT IS WORKING

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -5,13 +5,8 @@ $('#postInputForm').on('submit', (e) => {
 	e.preventDefault();
 	console.log("button pushed");
 	console.log($('#postInput').val());
-	$('li').append($('#postInput').val());
-	
-	//.append($('<li></li>'));
-	// $('li').addTo('ul')
-
-
-	// $('li')append($("#postInput").val());
+	$('ul').append("<li>" + $('#postInput').val() + "</li>");
+	$('#postInput').val('');
 });
-	// create new li
-	// append to ul
+	
+
