cb85f52b7ca42e812a505959161b088760d9a4b8
thegreatsantini <lss13@my.fsu.edu>
2018-03-30 11:14:58 -0700

    initial commit

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,1 +1,20 @@
-// Your jQuery Logic will go here
+const addNewMovie = (e) => {
+    if (e.which === 13) {
+        console.log(e.target.value)
+    }  else if (e.type === 'click') {
+        const newMovie = $('input:first').val()
+        console.log(newMovie)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+$('input').keypress(addNewMovie)
+$("input[type='submit']").click(addNewMovie)
