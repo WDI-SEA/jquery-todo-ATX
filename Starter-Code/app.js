d8d8978883a39d88d7dc91b9d72b92a6c1cd12c1
jessiheger <jhmheger@gmail.com>
2018-04-02 11:39:21 -0700

    my commit

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,38 +1,17 @@
-// Your jQuery Logic will go here
-
-// $('#addForm').submit(function (e) {
-//  e.preventDefault();
-//  console.log('submitted!');
-// });
-// const inputBox = $('addForm');
 
 $(document).ready(function(){
-$('button').click( (e) => {
-  e.preventDefault();
-  var taskText = $("#addTask").val();
-  var newLI = $('<li>');
-  newLI.on('click', liMove);
-  $('#outstanding').append(newLI);
-  $(newLI).html(taskText);
-  $('#addTask').val('');
-})
+	$('button').click( (e) => {
+  		e.preventDefault();
+		var newLI = $('<li>');
+		newLI.on('click', liMove);
+		$('#jfdi').append(newLI);
+		$(newLI).html($("#addTask").val());
+		$('#addTask').val('');
+	})
 });
 
-
-$(document).ready(function(){
-// $('li').click( (e) => {
-//  // e.preventDefault();
-//  var taskDone = e.target;
-//  var lIDone = document.createElement('li');
-//  $('#done').append(lIDone);
-//  $(lIDone).html(taskDone);
-// })
-})
-
 function liMove(e){
-  $(this).appendTo($('#done'));
+	$(this).appendTo($('#doneAF'));
 }
 
-// add click event to item in first column
 
-// once firstArray item is clicked, splice out of firstArray and add to secondArray.
