d45686ae2deaa0b9b1bf577508d90f508eb8d687
thegreatsantini <lss13@my.fsu.edu>
2018-04-02 09:21:04 -0700

    started localstorage debugging

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,59 +1,63 @@
-$(document).ready( ()=> {
+const getLocalStorage = () => {
+    let savedPostsStr = localStorage.getItem('movieList');
+    if (savedPostsStr) {
+        movieList = JSON.parse(savedPostsStr);
+        for (let key in movieList ) {
+            console.log(movieList[key])
+            $('#moviesUl').append(`<li class="listItem">${movieList[key].title}<span><i class="fas fa-check "></i></span></li>`);
+        };
+    };
+}
+
+$(document).ready(() => {
     getLocalStorage()
     // checkForDeletions()
     // updateLists()
 
-})
-let movieList = {};
 
+    let movieList = {};
 
-const setLocalStorage = (entriesObj) => {
-    localStorage.setItem('movieList', JSON.stringify(entriesObj));
- }
-
- const getLocalStorage = () => {
-    let savedPostsStr = localStorage.getItem('movieList');
-    if (savedPostsStr) {
-       movieList = JSON.parse(savedPostsStr);
-       for (let key in movieList) {
-          $('#moviesUl').append(`<li class="listItem">${movieList[key].title}<span><i class="fas fa-check "></i></span></li>`);
-       };
-    };
- }
-
-
-// add new movie title to watch 
-$('#addForm').submit((e) => {
-    e.preventDefault()
+    const setLocalStorage = (entriesObj) => {
+        console.log(entriesObj)
+        localStorage.setItem('movieList', JSON.stringify(entriesObj));
+    }
     
-    const newMovie = $('#search').val()
 
-    movieList.timeStamp = e.timeStamp
-    movieList.title = $('#search').val() 
-    $('#search').val('')
-    $('#movieUl').append
-    (   // does jQuery not accept template literals? idk why it worked ealier
-        '<li class="listItem">' + newMovie + '<span><i class="fas fa-check "></i></span></li>'
-    )
+    
 
-    // add new item to local storage
-    setLocalStorage(newMovie)
+
+    // add new movie title to watch 
+    $('#addForm').submit((e) => {
+        e.preventDefault()
+
+        const newMovie = $('#search').val()
+
+        movieList.timeStamp = e.timeStamp
+        movieList.title = $('#search').val()
+        $('#search').val('')
+        $('#movieUl').append
+            (   // does jQuery not accept template literals? idk why it worked ealier
+            '<li class="listItem">' + newMovie + '<span><i class="fas fa-check "></i></span></li>'
+            )
+            console.log(newMovie)
+        // add new item to local storage
+        setLocalStorage(newMovie)
+    })
+
+    // check off watched film by clicking
+    $('ul').on('click', 'li', function () {
+        console.log(this)
+        $(this).toggleClass('completed')
+    })
+
+    // click check to delete. I had trouble targeting the span element without 
+    // using es5 function and using 'this' keyword
+    $('ul').on('click', 'span', function () {
+        $(this)
+            .parent()
+            .fadeOut(500, function () {
+                $(this).remove();
+            });
+    })
+
 })
-
-// check off watched film by clicking
-$('ul').on('click', 'li', function() {
-    console.log(this)
-    $(this).toggleClass('completed')
-})
-
-// click check to delete. I had trouble targeting the span element without 
-// using es5 function and using 'this' keyword
-$('ul').on('click', 'span', function() {
-    $(this)
-        .parent()
-        .fadeOut(500, function(){
-            $(this).remove();
-        });
-})
-
-
